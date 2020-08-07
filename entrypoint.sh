e4b0dcc30440524c400f321f5d54c7adbc3d8d8d
TomKristie <65685417+TomKristie@users.noreply.github.com>
2020-08-07 18:42:35 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-for VAR in 1 2 .. 11
-do
-  echo $VAR
-done
+echo $1
+echo $2
+echo $11
 
-code-suggester code-suggester $1 --upstream-repo=$2 --upstream-owner=$3 --description=$4 --title=$5 --branch=$6 --primary=$7 --message=$8 --force=$9 --maintainers-can-modify=$10 --git-dir=$11
+code-suggester $1 --upstream-repo=$2 --upstream-owner=$3 --description=$4 --title=$5 --branch=$6 --primary=$7 --message=$8 --force=$9 --maintainers-can-modify=$10 --git-dir=$11
