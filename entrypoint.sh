2abb1d28678ffd34eacc5fa8463c2b1f5e3909bf
TomKristie <65685417+TomKristie@users.noreply.github.com>
2020-08-07 19:27:04 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
-
-echo $1
-echo $2
-echo $INPUT_GIT_DIR
-ls
-
 code-suggester $INPUT_COMMAND \
---upstream-repo=$INPUT_UPSTREAM_REPO \
---upstream-owner=$INPUT_UPSTREAM_OWNER \
---description=$INPUT_DESCRIPTION \
---title=$INPUT_TITLE \
---branch=$INPUT_BRANCH \
---primary=$INPUT_PRIMARY \
---message=$INPUT_MESSAGE \
---force=$INPUT_FORCE \
---maintainers-can-modify=$INPUT_MAINTAINERS_CAN_MODIFY \
---git-dir=$INPUT_GIT_DIR
+--upstream-repo="$INPUT_UPSTREAM_REPO" \
+--upstream-owner="$INPUT_UPSTREAM_OWNER" \
+--description="$INPUT_DESCRIPTION" \
+--title="$INPUT_TITLE" \
+--branch="$INPUT_BRANCH" \
+--primary="$INPUT_PRIMARY" \
+--message="$INPUT_MESSAGE" \
+--force="$INPUT_FORCE" \
+--maintainers-can-modify="$INPUT_MAINTAINERS_CAN_MODIFY" \
+--git-dir="$INPUT_GIT_DIR"
